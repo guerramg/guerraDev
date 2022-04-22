ad34f153a5712bb6c4a9dcc6a518832ed1a9ac11
guerramg <agenciaintuito@gmail.com>
2022-04-22 20:09:09 -0300

    ajustes no css e html

--- envia.php
+++ envia.php
@@ -1,19 +1,61 @@
 <?php
+//Variáveis
 
-    $to = "webphp@yahoo.com.br";
-    $from = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $name = $_REQUEST['name'];
-    $headers = "From: $from";
+$nome = $_POST['name'];
+$whatsapp = $_POST['email'];
+$mensagem = $_POST['message'];
+// Corpo E-mail
+$arquivo = "
+<style type='text/css'>
+body {
+margin:0px;
+font-family:Verdane;
+font-size:12px;
+color: #666666;
+}
+a{
+color: #666666;
+text-decoration: none;
+}
+a:hover {
+color: #FF0000;
+text-decoration: none;
+}
+</style>
+  <html>
+      <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+          <tr>
+            <td>
+<tr>
+               <td width='500'>Nome:$nome</td>
+              </tr>
+              <tr>
+                <td width='320'>Whatsapp:<b>$email</b></td>
+   </tr>
+                <td width='320'>Mensagem:$mensagem</td>
+              </tr>
+      </table>
+  </html>
+";
+//enviar
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"subject"} = "subject";
-    $fields{"message"} = "message";
+  // emails para quem será enviado o formulário
+  $emailenviar = "agenciaintuito@gmail.com";
+  $destino = $emailenviar;
+  $assunto = "Contato do Site Guerra DEV";
 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+      $headers .= 'From: '.$nome.' <'.$email.'>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
 
-    $send = mail($to, $subject, $body, $headers);
-
+  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+  if($enviaremail){
+    print "<script>alert('Email enviado com sucesso!')</script>";
+    print "<script>location=('https://dev.guerra.app.br')</script>";
+  } else {
+    print "<script>alert('ERRO! Email não enviado!')</script>";
+    print "<script>location=('https://dev.guerra.app.br')</script>";
+  }
 ?>
